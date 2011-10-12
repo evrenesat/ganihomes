073dfbd91def693b18dd12b6d62efaae72c5a7bd
Evren Esat Ozkan <esat@elipsis.com.tr>
2011-10-12 23:47:07 +0300

    ~

--- places/models.py
+++ places/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from options import *
 
-# Create your models here.
+
+class Places(models.Model):
+    '''Places'''
+
+    title = models.CharField(_('Place title'))
+    type = models.SmallIntegerField(_('Place type'), choices=PLACE_TYPES)
+    space = models.SmallIntegerField(_('Space offered'), choices=SPACE_TYPES)
+    bedroom = models.SmallIntegerField(_('Number of bedrooms'), choices=NO_OF_ROOMS)
+    bed_type = models.SmallIntegerField(_('Bed type'), choices=BATHROOM_TYPES)
+    bathroms = models.SmallIntegerField(_('Number of bathrooms'), choices=NO_OF_ROOMS)
+    size = models.IntegerField(_('Size'))
+    pets = models.BooleanField(_('Pets'))
+
+    cancelation = models.SmallIntegerField(_('Cancellation rules'), choices=CANCELATION_RULES)
+    min_stay = models.SmallIntegerField(_('Minimum number of nights'), choices=MAX_STAY)
+    max_stay = models.SmallIntegerField(_('Maximum number of nights'), choices=MIN_STAY)
+    manual = models.TextField(_('House manual'))
+    rules = models.TextField(_('House rules'))
+
+    price = models.DecimalField(_('Price per night'), help_text=_('Price for guest'))
+
+
+    # = models.ForeignKey(, verbose_name=_(''))
+    # = models.CharField(_(''))
+    # = models.IntegerField(_(''))
+    # = models.SmallIntegerField(_(''))
+    timestamp = models.DateTimeField(_('timestamp'))
+
+    class Meta:
+        ordering = ['timestamp']
+        get_latest_by = "timestamp"
+        #verbose_name = _('')
+        #verbose_name_plural = _('')
+
+    def __unicode__(self):
+        return '%s' % (self.title,)
---
+++ places/options.py
@@ -0,0 +1,45 @@
+__author__ = 'Evren Esat Ozkan'
+from django.utils.translation import ugettext_lazy as _
+
+n_tuple = lambda n: tuple([(i,i) for i in range(1, n)])
+
+PLACE_TYPES = (
+    (1, _('Apartment')),
+    (2, _('House')),
+    (3, _('Garden House')),
+    (4, _('Bed and Breakfast')),
+    (5, _('Villa')),
+    (6, _('Caravan')),
+)
+SPACE_TYPES = (
+    (1, _('Entire Place')),
+    (2, _('Private Room')),
+    (3, _('Shared Room')),
+)
+
+
+
+NO_OF_ROOMS = n_tuple(10)
+MIN_STAY = n_tuple(7)
+MAX_STAY = n_tuple(7)
+NO_OF_BEDS = n_tuple(20)
+
+BED_TYPES = (
+    (1, _('Real bed')),
+    (2, _('Bunk beds')),
+    (3, _('Waterbed')),
+    (4, _('Hammock')),
+    (5, _('Couch')),
+)
+
+BATHROOM_TYPES = (
+    (1, _('Private')),
+    (2, _('Shared')),
+)
+
+CANCELATION_RULES = (
+    (1, _('Flexible')),
+    (2, _('Semi-flexible')),
+    (3, _('Strict')),
+)
+
