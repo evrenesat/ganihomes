59f6284b2f028ab1569ce146815c3f5fd951c1eb
Evren Esat Ozkan <sleytr@gmail.com>
2011-12-14 03:41:25 +0200

    ~

---
+++ localeurl/settings.py
@@ -0,0 +1,23 @@
+import re
+from django.conf import settings
+
+SUPPORTED_LOCALES = dict(settings.LANGUAGES)
+# Issue #15. Sort locale codes to avoid matching e.g. 'pt' before 'pt-br'
+LOCALES_RE = '|'.join(
+    sorted(SUPPORTED_LOCALES.keys(), key=lambda i: len(i), reverse=True))
+PATH_RE = re.compile(r'^/(?P<locale>%s)(?P<path>.*)$' % LOCALES_RE)
+
+LOCALE_INDEPENDENT_PATHS = [re.compile(p) for p in
+                            getattr(settings, 'LOCALE_INDEPENDENT_PATHS', [])]
+
+LOCALE_INDEPENDENT_MEDIA_URL = getattr(settings,
+        'LOCALE_INDEPENDENT_MEDIA_URL', True)
+
+LOCALE_INDEPENDENT_STATIC_URL = getattr(settings,
+        'LOCALE_INDEPENDENT_STATIC_URL', True)
+
+PREFIX_DEFAULT_LOCALE = getattr(settings, 'PREFIX_DEFAULT_LOCALE', True)
+
+USE_ACCEPT_LANGUAGE = getattr(settings, 'LOCALEURL_USE_ACCEPT_LANGUAGE', True)
+
+LOCALE_REDIRECT_PERMANENT = getattr(settings, 'LOCALE_REDIRECT_PERMANENT', True)
