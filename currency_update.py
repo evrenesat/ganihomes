d748bb0da45aecf65f25a0535bf1c88750e7eec1
Evren Esat Ozkan <sleytr@gmail.com>
2012-02-07 10:20:18 +0200

    p

--- currency_update.py
+++ currency_update.py
@@ -1,23 +1,18 @@
 #! /usr/bin/env python
 # -*-  coding: utf-8 -*-
-import sys
-import os
+import os,sys
 pathname = os.path.dirname(sys.argv[0])
 sys.path.append(os.path.abspath(pathname))
 sys.path.append(os.path.normpath(os.path.join(os.path.abspath(pathname), '../')))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 from applicationinstance import ApplicationInstance
 
-from apiclient.discovery import build
-from places.models import Currency
-#from utils.cache import kes
 
+from places.models import Currency
 
 
 import logging
 log = logging.getLogger('genel')
-
-import dbsettings
 
 
 
